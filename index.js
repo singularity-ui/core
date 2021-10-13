70d8f3340a4c28e9dfe224883c4a65c9b33faf98
Ivan Gabriele <ivan.gabriele@gmail.com>
2021-10-13 13:11:12 +0200

    fix(build): index Card

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import Table from './contents/Table'
 import Tasker from './contents/Tasker'
 import Button from './elements/Button'
+import Card from './elements/Card'
 import Select from './fields/Select'
 import TextInput from './fields/TextInput'
 import GlobalStyle from './GlobalStyle'
 import ThemeProvider from './ThemeProvider'
 
-export default { Button, GlobalStyle, Select, Table, Tasker, TextInput, ThemeProvider }
+export default { Button, Card, GlobalStyle, Select, Table, Tasker, TextInput, ThemeProvider }
