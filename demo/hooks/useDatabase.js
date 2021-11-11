fec7746dc46356e110c0131ea4f9db3ce9c635a0
Ivan Gabriele <ivan.gabriele@gmail.com>
2021-11-12 00:19:13 +0100

    build(demo): delete demo package

--- demo/hooks/useDatabase.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import Context from '../hocs/withDatabase/Context'
-
-export default function useDatabase() {
-  const contextValue = React.useContext(Context)
-
-  return contextValue
-}
