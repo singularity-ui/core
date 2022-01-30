3645b62b8d12c07abe1d0a066e8769233e0f489b
Ivan Gabriele <ivan.gabriele@gmail.com>
2022-01-30 18:17:32 +0100

    feat(elements): add className on Card

--- elements/Card.tsx
+++ elements/Card.tsx
@@ -1,11 +1,24 @@
+import React from 'react'
 import styled from 'styled-components'
 
-import type { DefaultTheme, StyledComponent } from 'styled-components'
+import type { ForwardRefRenderFunction, HTMLAttributes } from 'react'
 
-export type CardProps = StyledComponent<'div', DefaultTheme, {}, never>
-export const Card: CardProps = styled.div`
+const StyledCard = styled.div`
   background-color: ${p => p.theme.color.card.background};
   border-radius: ${p => p.theme.appearance.borderRadius.large};
   box-shadow: ${p => p.theme.shadow.small};
   padding: ${p => p.theme.padding.layout.medium};
 `
+
+export type CardProps = HTMLAttributes<HTMLDivElement>
+const CardWithRef: ForwardRefRenderFunction<HTMLDivElement, CardProps> = ({ children, ...props }: CardProps, ref) => (
+  <StyledCard ref={ref} className="Card" {...props}>
+    {children}
+  </StyledCard>
+)
+
+export const Card = React.forwardRef(CardWithRef)
+
+Card.displayName = 'Card'
+
+Card.propTypes = {}
--- stories/elements/Card.stories.tsx
+++ stories/elements/Card.stories.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import { Card } from '../..'
+
+import type { CardProps } from '../..'
+
+const Box = styled.div`
+  max-width: 30rem;
+`
+
+const args: CardProps = {}
 
 export default {
   title: 'Elements/Card',
   component: Card,
+
+  argTypes: {},
+
+  ...args,
 }
 
-export const _Card = (props: any) => <Card {...props}>This is just a card.</Card>
+export const _Card = (props: CardProps) => (
+  <Box>
+    <Card {...props}>This is just a card.</Card>
+  </Box>
+)
