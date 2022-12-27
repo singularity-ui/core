81fef12cf75b1fc8907e5c8ba20a4953a67ef206
Ivan Gabriele <ivan.gabriele@gmail.com>
2022-12-27 10:05:23 +0100

    fix(storybook)!: migrate to Webpack v5
    BREAKING CHANGE: Remove better-prop-types from dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,22 @@
 /* eslint-env node */
 
 module.exports = {
-  addons: [
-    '@storybook/addon-actions',
-    '@storybook/addon-essentials',
-    '@storybook/addon-links',
-    '@storybook/addon-postcss',
-  ],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-a11y', 'storybook-addon-performance'],
+  core: {
+    builder: 'webpack5',
+  },
   staticDirs: ['../public'],
   stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  // Messy issue with assert resolving (related to docs generation), fixed in Storybook v7.
+  // https://github.com/storybookjs/storybook/issues/17458#issuecomment-1268464401
+  webpackFinal: config => ({
+    ...config,
+    resolve: {
+      ...config.resolve,
+      fallback: {
+        ...config.resolve.fallback,
+        assert: require.resolve('browser-assert'),
+      },
+    },
+  }),
 }
